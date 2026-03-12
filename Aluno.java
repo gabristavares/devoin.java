89717836cca6e89d0a146faf93aacde801234bad
gabristavares <soarestavaresgabriel@gmail.com>
2026-03-11 21:02:07 -0300

    projeto apostila 3

---
+++ Aluno.java
@@ -0,0 +1,15 @@
+public class Aluno {
+
+    //Definir os atributos do aluno
+    short idade;
+    String nomeCurso;
+    boolean matriculado;
+    double valorMensalidade;
+    char turma;
+    int id;
+    String nome;
+
+
+
+
+}
---
+++ Master.java
@@ -0,0 +1,20 @@
+public class Master {
+    public static void main(String[] args) {
+        System.out.println("Bom dia");
+
+        //Declarar variaveis no java
+        short idade;
+        //Atribuindo um valor para variavel
+        idade = 32;
+        //Declarar a variavel e atribuir um valor
+        int anoNascimento = 1994;
+        double salario =16000.50;
+        float altura =16000.50f;
+        char inicalNome = 'G';
+        boolean maiorIdade = true; //false
+        String nomeDele = "Gabriel";
+    }
+
+
+
+}
